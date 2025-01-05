35cef8c6870ac56364b1a90e035592132b797c2c
Rob Aimes <rob@aimes.dev>
2025-01-05 15:31:04 +0000

    Remove choice() override

--- Console/Style/SubstratumStyle.php
+++ Console/Style/SubstratumStyle.php
@@ -7,9 +7,9 @@
 
 namespace Aimes\Substratum\Console\Style;
 
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ChoiceQuestionFactory;
 use Symfony\Component\Console\Style\StyleInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -21,32 +21,11 @@ class SubstratumStyle extends SymfonyStyle implements StyleInterface
     public function __construct(
         InputInterface $input,
         OutputInterface $output,
-        private readonly ChoiceQuestionFactory $choiceQuestionFactory,
     ) {
         parent::__construct($input, $output);
     }
 
-    public function choice(string $question, array $choices, $default = null, bool $multiple = false)
-    {
-        if (null !== $default) {
-            $values = array_flip($choices);
-            $default = $values[$default] ?? $default;
-        }
-
-        $choiceQuestion = $this->choiceQuestionFactory->create([
-            'question' => $question,
-            'choices' => $choices,
-            'default' => $default,
-        ]);
-
-        if ($multiple) {
-            $choiceQuestion->setMultiselect(true);
-        }
-
-        return $this->askQuestion($choiceQuestion);
-    }
-
-    public function createProgressBar(int $max = 0, $format = self::DEFAULT_PROGRESS_FORMAT)
+    public function createProgressBar(int $max = 0, $format = self::DEFAULT_PROGRESS_FORMAT): ProgressBar
     {
         $progressBar = parent::createProgressBar($max);
 
